8afd513b33dd476ced4ac5de5fefa91209e0b838
milocai <61470383+milocai@users.noreply.github.com>
2023-08-31 22:09:28 -0300

    customize template

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,12 +1,12 @@
 // Personal Information --> #hero section
-export const name = "Peter Griffin"
-export const designation = "Safety Inspector"
-export const location = "Quahog, Rhode Island"
+export const name = "Sergio Rubino"
+export const designation = "Frontend Developer"
+export const location = "Tucumán, Argentina"
 export const pronouns = "he/him"
-export const website = "https://petergriffin.com"
+export const website = "https://github.com/milocai"
 
 // About
-export const about = "An adventurous man working as a safety inspector with a quirky sense of humor. Husband to Louis Griffin. Father of Chris, Brian, and Stewie."
+export const about = "Advanced Information Systems Engineering student. Currently looking for my supervised internship and doing my thesis. Passionate about the use and research of new technologies, interested in various areas of the software industry, particularly frontend development."
 
 // Work Experience --> #work section
 export const work = [
@@ -21,66 +21,97 @@ export const work = [
 	}
 ]
 
+export const skills = [
+	{
+		title: "Agile Methodologies",
+		description: "Scrum and XP",
+	},
+	{
+		title: "Version Control Systems",
+		description: "Git and GitHub",
+	},
+	{
+		title: "Data analysis, data transformation and presentation tools",
+		description: "PowerBI, Excel, Google Sheets",
+	},
+	{
+		title: "Automated testing",
+		description: "Katalon, Playwright",
+	}, {
+		title: "Networking",
+		description: "LAN and WLAN administration and configuration",
+	},
+	{
+		title: "Microsoft Active Directory servers and services configuration",
+		description: "DNS, DHCP, Group Policy, File Server, Print Server, etc.",
+	},
+	{
+		title: "LAMP stack deployment on virtual environments",
+		description: "Linux, Apache, MySQL, PHP; Proxmox",
+	},
+	{
+		title: "Languages and technologies",
+		description: "UML, Java, HTML, CSS, Javascript, Node, Express, Next, React, Bootstrap, Material UI, MySQL and MongoDB",
+	},
+	{
+		title: "Soft skills",
+		description: "Teamwork, communication, leadership, problem solving, empathy, negotiation, strategic and critical thinking",
+	},
+]
+
 // Projects --> #project section
 export const projects = [
 	{
-		title: "Project Griffin",
-		description: "A project to improve safety standards in Quahog by implementing innovative safety measures.",
-		url: "https://projectgriffin.com",
-		tech: "Safety Equipment, Training Programs, Community Outreach",
+		title: "Sistema de Gestión de esquemas de trabajo",
+		description: "A project to improve the work schedule of Sanatorio 9 de Julio's employees",
+		url: "https://github.com/vab1997/s9j-app",
+		tech: "Full stack web development, E2E testing, CI/CD, Agile methodologies and VCS",
 	},
 	{
-		title: "Family Safety Campaign",
-		description: "An initiative to promote safety awareness among families in Quahog.",
-		url: "https://safefamilyquahog.com",
-		tech: "Workshops, Educational Material, Public Events",
+		title: "My resume",
+		description: "A project to improve my skills as a frontend developer",
+		url: "https://milocai.github.io/my-resume/",
+		tech: "Front end development, Next.js, React.js, CI/CD, and VCS",
 	},
 	{
-		title: "Road Safety Initiative",
-		description: "A campaign to enhance road safety and reduce accidents in Quahog.",
-		url: "https://roadsafetyquahog.com",
-		tech: "Traffic Analysis, Signage Improvement, Public Awareness",
-	}
+		title: "JavaScript Quiz",
+		description: "Another project to improve my skills as a frontend developer",
+		url: "https://milocai.github.io/js-quiz/",
+		tech: "Front end development, Vite, React.js, zustand, CI/CD, and VCS",
+	},
 ]
 
 // Education --> #education section
 export const education = [
 	{
-		from: "1995",
-		to: "1999",
-		institute: "Quahog High School",
-		course: "High School Diploma",
-		location: "Quahog, Rhode Island"
+		from: "2009",
+		to: "2016",
+		institute: "Agrotécnico 20 de Junio",
+		course: "Agricultural Production Technician",
+		location: "Lules, Tucumán"
 	},
 	{
-		from: "1999",
-		to: "2003",
-		institute: "Quahog Community College",
-		course: "Bachelor of Science in Safety Management",
-		location: "Quahog, Rhode Island"
+		from: "2017",
+		to: "2023",
+		institute: "Universidad Tecnológica Nacional - Facultad Regional Tucumán",
+		course: "Information Systems Engineering",
+		location: "San Miguel de Tucumán, Tucumán"
 	},
-	{
-		from: "2003",
-		to: "2005",
-		institute: "Safety University",
-		course: "Master of Science in Safety Engineering",
-		location: "Boston, Massachusetts"
-	}
 ]
 
 // Contact --> #contact section
 export const contact = [
 	{
 		source_name: "Email",
-		source: "peter.griffin@example.com",
+		source: "srg.rbn7@gmail.com",
 	},
 	{
 		source_name: "Twitter",
-		source: "https://twitter.com/PeterGriffin",
+		source: "https://twitter.com/miloifc",
 	},
 	{
 		source_name: "LinkedIn",
-		source: "https://www.linkedin.com/in/peter-griffin",
+		source: "https://www.linkedin.com/in/sarubino/",
 	},
 ]
 
@@ -88,26 +119,26 @@ export const contact = [
 
 export const certificates = [
 	{
-		issued: "2005",
-		title: "Certified Safety Inspector",
-		org: "National Safety Association",
-		url: "https://certi.com/url",
-		description: "Certification in safety inspection and risk assessment.",
+		issued: "2022",
+		title: "Scrum Foundation Professional Certification - SFPC™",
+		org: "CertiProf",
+		url: "https://www.credly.com/badges/2c9f94c4-3933-4f97-b7c2-a17a97c83033",
+		description: "Scrum Foundation Professional Certification holders have developed entry-level skills in Scrum that endorse their fundamental knowledge in this framework. They have demonstrated an understanding of the empirical Scrum pillars of transparency, inspection, and adaptation. Their primary focus is on the work of Sprint to make the best possible progress toward these goals.",
 	},
-	{
-		issued: "2006",
-		title: "Emergency Response Training",
-		org: "Quahog Safety Department",
-		url: "https://certi.com/url",
-		description: "Training in handling emergency situations and crisis management.",
-	},
-	{
-		issued: "2007",
-		title: "Safety Leadership Workshop",
-		org: "Safety Leaders International",
-		url: "https://certi.com/url",
-		description: "Leadership training in safety management and communication.",
-	}
+	// {
+	// 	issued: "2006",
+	// 	title: "Emergency Response Training",
+	// 	org: "Quahog Safety Department",
+	// 	url: "https://certi.com/url",
+	// 	description: "Training in handling emergency situations and crisis management.",
+	// },
+	// {
+	// 	issued: "2007",
+	// 	title: "Safety Leadership Workshop",
+	// 	org: "Safety Leaders International",
+	// 	url: "https://certi.com/url",
+	// 	description: "Leadership training in safety management and communication.",
+	// }
 ]
 
 // Blogs --> #blogs section
@@ -131,23 +162,8 @@ export const blogs = [
 // Achievements --> #achievement section
 export const achievements = [
 	{
-		title: "Safety Hero Award",
-		year: "2020",
-		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
+		title: "Second flag escort of the National Flag",
+		year: "2021",
+		decription: "Recognized for having the third best average of the Facultad Regional Tucumán (UTN): Partial average: 8.47."
 	},
-	{
-		title: "Safety Innovation Prize",
-		year: "2018",
-		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
-	},
-	{
-		title: "Community Safety Ambassador",
-		year: "2015",
-		decription: "Honored for promoting safety awareness and education in the Quahog community."
-	},
-	{
-		title: "Safety Campaign Excellence",
-		year: "2013",
-		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
-	}
 ]
